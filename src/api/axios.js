0fc355ba8d2291ec883b749b3d4f0e9793fa4b26
klu2400032417 <2400032417@kluniversity.in>
2026-04-30 21:45:24 +0530

    updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "https://fsad-sdp-backend-needbridge.up.railway.app/";
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  "https://fsad-sdp-backend-needbridge.up.railway.app";
 
 const API = axios.create({
   baseURL,
